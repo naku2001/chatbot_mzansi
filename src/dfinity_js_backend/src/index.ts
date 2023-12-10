cb59dc68fdb16a9ed426b372d1de885a7bc880ed
Perfect-Princess Makuwerere <93464813+naku2001@users.noreply.github.com>
2023-12-10 12:04:56 +0200

    Update index.ts

--- src/dfinity_js_backend/src/index.ts
+++ src/dfinity_js_backend/src/index.ts
@@ -1,12 +1,12 @@
 import { Canister } from "azle";
 import assistant from "./assistant";
 import user from "./user";
-import { ASSISTANT_ID } from "../../../credential";
+// import { ASSISTANT_ID } from "../../../credential";
 
-let assistantId: string = ASSISTANT_ID ?? "";
+// let assistantId: string = ASSISTANT_ID ?? "";
 
 export default Canister({
-  getAssistant: assistant.getAssistant(assistantId),
+  // getAssistant: assistant.getAssistant(assistantId),
   updateUsername: user.updateUsername(),
   getUsername: user.getUsername(),
   saveThread: assistant.saveThread(),
